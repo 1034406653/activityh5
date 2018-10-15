6ed5956e200a29e0e6a924f0deef229b5c67460d
lxl <1034406653@qq.com>
2018-10-15 09:43:21 +0800

    lxl

---
+++ gaming/pc/dist/js/language.js
@@ -0,0 +1,119 @@
+var chineseData = {
+    "headTitle": {
+        "languageActive": "中文",
+        "languageList":["中文","英文"],
+        "headTitle02": "怎么玩",
+        "headTitle03": "登出"
+    },
+    "betKind": {
+        "bet01": "大小单双",
+        "bet02": "和值",
+        "bet03": "三同号",
+        "bet01Kind": {
+            "bet01Kind01": "大",
+            "bet01Kind02": "小",
+            "bet01Kind03": "单",
+            "bet01Kind04": "双",
+        }
+    },
+    "betCurrency": {
+        "betCurrency": "投注金额",
+        "betBalance": "余额",
+        "betPredict": "预计赢得金额",
+        "betDice": "掷骰子"
+    },
+    "betDataList": {
+        "aLLBet": "所有投注",
+        "perBet": "个人投注",
+        "betTime": "时间",
+        "betCurrency": "投注",
+        "betEvent": "事件",
+        "betBonus": "奖金"
+    },
+    "noMore": "没有更多了",
+    "toast": {
+        "toastLess": "必须至少投注",
+        "toastNoEnough": "此投注没有足够EOS",
+        "toastSuccess": "投注成功",
+        "toastGet": "获得",
+        "toastLose": "再接再厉",
+        "toastGoing": "开盘中",
+        "toastLack": "您的CPU不足,",
+        "toastLackText": "点击充值"
+    },
+    "guideTitle": "怎么玩",
+    "guide": [
+        "1、确保您拥有EOS账户，有关如何如何创建账户的教程，(a1)请点击此处(/a1);",
+        "2、如果您还没有，请下载并(a2)安装Scatter(/a2)，这是一个EOS钱包，可以促进用户和Dapp之间的交互;",
+        "3、设置您的下注EOS数量，也可以调整滑块来设置下注额，单笔最高投注额为200 EOS;",
+        "4、选择预测的事件;",
+        "5、点击掷骰子进行下注;",
+        "6、如果三个骰子数字相加之和与您预测的事件相符，您就赢了;",
+        "7、您可以在页面下方查看全球独有玩家和自己的最近赌注;",
+        "8、如果您收到交易失败的通知，请检查您是否有足够的CPU和带宽来进行交易！"
+    ]
+}
+
+var englishData = {
+    "headTitle": {
+        "languageActive": "english",
+        "languageList":["chinese","english"],
+        "headTitle02": "guide",
+        "headTitle03": "login"
+    },
+    "betKind": {
+        "bet01": "大小单双",
+        "bet02": "和值",
+        "bet03": "三同号",
+        "bet01Kind": {
+            "bet01Kind01": "大",
+            "bet01Kind02": "小",
+            "bet01Kind03": "单",
+            "bet01Kind04": "双",
+        }
+    },
+    "betCurrency": {
+        "betCurrency": "投注金额",
+        "betBalance": "余额",
+        "betPredict": "预计赢得金额",
+        "betDice": "掷骰子"
+    },
+    "betDataList": {
+        "aLLBet": "所有投注",
+        "perBet": "个人投注",
+        "betTime": "时间",
+        "betCurrency": "投注",
+        "betEvent": "事件",
+        "betBonus": "奖金"
+    },
+    "noMore": "没有更多了",
+    "toast": {
+        "toastLess": "必须至少投注",
+        "toastNoEnough": "此投注没有足够EOS",
+        "toastSuccess": "投注成功",
+        "toastGet": "获得",
+        "toastLose": "再接再厉",
+        "toastGoing": "开盘中",
+        "toastLack": "您的CPU不足,",
+        "toastLackText": "点击充值"
+    },
+    "guideTitle": "怎么玩",
+    "guide": [
+        "1、确保您拥有EOS账户，有关如何如何创建账户的教程，(a1)请点击此处(/a1);",
+        "2、如果您还没有，请下载并(a1)安装Scatter(/a2)，这是一个EOS钱包，可以促进用户和Dapp之间的交互;",
+        "3、设置您的下注EOS数量，也可以调整滑块来设置下注额，单笔最高投注额为200 EOS;",
+        "4、选择预测的事件;",
+        "5、点击掷骰子进行下注;",
+        "6、如果三个骰子数字相加之和与您预测的事件相符，您就赢了;",
+        "7、您可以在页面下方查看全球独有玩家和自己的最近赌注;",
+        "8、如果您收到交易失败的通知，请检查您是否有足够的CPU和带宽来进行交易！"
+    ]
+}
+var activeData=chineseData;
+
+
+
+
+
+
+
